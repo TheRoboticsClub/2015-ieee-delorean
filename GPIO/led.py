435792e67f1547fc5782b250621d634f0588c2dc
agvergara <vergantonio93@gmail.com>
2017-02-15 16:57:53 +0100

    prueba de leds

--- GPIO/led.py
+++ GPIO/led.py
@@ -5,11 +5,21 @@ led = 5
 
 
 GPIO.setup(led,GPIO.OUT)
+try:
 
-for i in range(0,5):
+	for i in range(0,5):
 
-	GPIO.output(led,1)
+		GPIO.output(led,1)
+		sleep(0.5)
+		GPIO.output(led,0)
 
-GPIO.cleanup
+	GPIO.output(led,0)
+	GPIO.cleanup
+	print ("El programa ha terminado de ejecutar")
+
+except keyboardinterrupt:
+	GPIO.output(led,0)
+	GPIO.cleanup
+	print("Se ha interrmpido la ejecucion")
 
 
