fb5a9d0db1a5a556514d2ccb814de2afa18704fc
miguelangel3 <miguelangel.abb@gmail.com>
2017-03-23 10:20:48 +0100

    Car_server_alfa, First steps to improve the arduino code

---
+++ Arduino/car_server_alfa/FUIVBALIJ7WO8T4/FUIVBALIJ7WO8T4.ino
@@ -0,0 +1,24 @@
+#include <SoftwareServo.h> 
+int potentiometer=A7;
+int potval;
+
+SoftwareServo ESC;
+
+void setup() {
+  
+  pinMode(potentiometer, INPUT);
+  ESC.attach(9);   
+  Serial.begin(9600);    
+  ESC.setMinimumPulse(800);
+  ESC.setMaximumPulse(2000);
+}
+
+void loop() {
+
+  potval=analogRead(potentiometer);
+  potval=map(potval,0,1023,0,180);
+  ESC.write(potval);
+  SoftwareServo::refresh();
+  Serial.println(potval);
+ }
+
