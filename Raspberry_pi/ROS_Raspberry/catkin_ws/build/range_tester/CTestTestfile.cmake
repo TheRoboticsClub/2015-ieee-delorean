bc1c1adba3ef6d0bb72937424464ae42583f3047
Adri <adri_rodri_64@hotmail.es>
2018-04-15 22:08:05 +0200

    reestructuring git

--- Raspberry_pi/ROS_Raspberry/catkin_ws/build/range_tester/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/delorean/Desktop/GitHub/2015-ieee-delorean/Raspberry_pi/ROS_Raspberry/catkin_ws/src/range_tester
-# Build directory: /home/delorean/Desktop/GitHub/2015-ieee-delorean/Raspberry_pi/ROS_Raspberry/catkin_ws/build/range_tester
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
