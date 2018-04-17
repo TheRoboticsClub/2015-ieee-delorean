1001383871b3a703e12c61da3b9196ed04638676
Adrián Rodrigo Castillo <adri_rodri_64@hotmail.es>
2018-04-18 00:29:33 +0200

    New ping protocol done, test pending

---
+++ Raspberry_Pi_Code/ROS_Raspberry/range_tester/scripts/testcar.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import rospy
+from bondpy import bondpy
+from std_msgs.msg import String
+import sys
+from geometry_msgs.msg import Twist
+import calendar
+import time
+import threading
+
+class Timer:
+
+
+    def __init__(self):
+        self.dt = 100
+        self.arrived = False
+        self.old_time = calendar.timegm(time.gmtime())
+        self.time_now = calendar.timegm(time.gmtime())
+
+
+def ping_verifier(counter):
+
+    while True:
+        if(counter.arrived):
+            #print('order arrived!')
+            counter.old_time = counter.time_now
+            counter.arrived = False
+
+        counter.time_now = calendar.timegm(time.gmtime())
+        counter.dt = counter.time_now - counter.old_time
+        print(counter.dt)
+        if(counter.dt >= 3):
+            print('stopping car!!!')
+            stop_msg = Twist()
+            stop_msg.linear.x = 0.5
+            stop_msg.angular.z = 0.5
+            #pub.publish(stop_msg)
+
+
+        time.sleep(1)
+
+
+def ping_receiver(data, counter):
+
+    counter.arrived = True
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data)
+    pub.publish(data)
+
+
+def listener(counter):
+
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    counter = Timer()
+    global pub
+    pub = rospy.Publisher('/arduino/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/ping", String, ping_receiver, counter)
+    rospy.Subscriber('/turtle1/cmd_vel', Twist, callback)
+    t = threading.Thread(target=ping_verifier, args=(counter,))
+    t.daemon = True
+    t.start()
+    listener(counter)
---
+++ Raspberry_Pi_Code/ROS_Raspberry/range_tester/scripts/testjoy_ubuntu.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+from std_msgs.msg import String
+from bondpy import bondpy
+import os
+import sys
+import time
+import threading
+
+
+def mapsteering(axis):
+    aux_axis = 0.5
+
+    if axis >= 0.0:
+        aux_axis = abs((axis/2.0) + 0.5)
+        if aux_axis >= 1.0:
+            return 1.0
+
+        else:
+            return aux_axis
+
+    else:
+        aux_axis = abs((abs(axis)/2.0) - 0.5)
+        if aux_axis <= 0.0:
+            return 0.0
+        else:
+            return aux_axis
+
+
+
+def mapthrottle(axis, currentgear):
+
+    aux_axis = 0.5
+    if axis >= 0.0:
+        aux_axis = abs((axis/2.0) + 0.5)
+
+        if currentgear == 0:
+            if aux_axis < 0.5417:
+                return aux_axis
+            else:
+                return 0.5417
+
+        elif currentgear == 1:
+            if aux_axis < 0.55:
+                return aux_axis
+            else:
+                return 0.55
+
+        elif currentgear == 2:
+            if aux_axis < 0.56:
+                return aux_axis
+            else:
+                return 0.56
+
+        else:
+            if aux_axis >= 1.0:
+                return 1.0
+
+            else:
+                return aux_axis
+
+    else:
+
+        aux_axis = abs((axis/2.0) + 0.5)
+
+        if currentgear == 0:
+            if aux_axis > 0.4:
+                return aux_axis
+            else:
+                return 0.4
+
+        elif currentgear == 1:
+            if aux_axis > 0.35:
+                return aux_axis
+            else:
+                return 0.35
+
+        elif currentgear == 2:
+            if aux_axis > 0.3:
+                return aux_axis
+            else:
+                return 0.3
+
+        else:
+            if aux_axis <= 0.0:
+                return 0.0
+
+            else:
+                return aux_axis
+
+
+def changegear(data, buttonstate):
+
+    geardown = data.buttons[4]
+    gearup = data.buttons[5]
+
+    if gearup == 1 and buttonstate.r1 == 0 and buttonstate.currentgear < 3:
+        rospy.loginfo('boton pulsado')
+        buttonstate.r1 = 1
+        buttonstate.currentgear += 1
+
+    elif gearup == 0 and buttonstate.r1 == 1:
+        rospy.loginfo('devuelvo el boton a 0')
+        buttonstate.r1 = 0
+
+
+    if geardown == 1 and buttonstate.l1 == 0 and buttonstate.currentgear > 0:
+        rospy.loginfo('boton pulsado')
+        buttonstate.l1 = 1
+        buttonstate.currentgear -= 1
+
+    elif geardown == 0 and buttonstate.l1 == 1:
+        rospy.loginfo('devuelvo el boton a 0')
+        buttonstate.l1 = 0
+
+
+    rospy.loginfo(buttonstate.currentgear)
+
+def callback(data, buttonstate):
+
+    changegear(data, buttonstate)
+    twist = Twist()
+    twist.linear.x = mapthrottle(data.axes[4], buttonstate.currentgear)
+    twist.angular.z = mapsteering(-data.axes[0])
+    rospy.loginfo(twist)
+    pub.publish(twist)
+
+def talker():
+
+    class button:
+        l1 = 0
+        r1 = 0
+        currentgear = 0
+
+
+    buttonstate = button()
+    buttonstate.r1 = 0
+    buttonstate.l1 = 0
+    buttonstate.currentgear = 0
+    rospy.Subscriber("joy", Joy, callback, buttonstate)
+    rospy.spin()
+
+
+def ping_sender(number):
+
+    while True:
+        hello_str = 'ping!'
+        #rospy.loginfo(hello_str)
+        pubping.publish(hello_str)
+        time.sleep(1)
+
+
+
+if __name__ == '__main__':
+    try:
+        global pubping
+        pubping = rospy.Publisher('/ping', String, queue_size=10)
+        global pub
+        pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+        t = threading.Thread(target=ping_sender, args=(0,))
+        t.daemon = True
+        t.start()
+        talker()
+    except rospy.ROSInterruptException:
+        pass
