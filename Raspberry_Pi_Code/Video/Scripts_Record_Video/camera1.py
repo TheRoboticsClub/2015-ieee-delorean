08bdb1aa8556cba222f484d04e0a922a05727d44
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2018-10-28 01:46:43 +0200

    First steps to record a video and save it in the raspberry .h264 format

---
+++ Raspberry_Pi_Code/Video/Scripts_Record_Video/camera1.py
@@ -0,0 +1,31 @@
+##
+import os
+from io import BytesIO
+from time import sleep
+import datetime as dt
+from picamera import PiCamera
+
+camera = PiCamera()
+##constants
+
+destination = '~/Vídeos'
+
+
+def cameraSettings():
+    camera.resolution = (1280, 720)
+    camera.video_stabilization = True
+
+
+def record():
+    filename = os.path.join(destination, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
+    camera.start_recording('filename','H264', 'none',1)
+
+def stopRecord():
+    camera.stop_recording(splitter_port = 1)
+
+if __name__ == '__main__':
+    ##camera = PiCamera()
+    cameraSettings()
+    record()
+    sleep(30)
+    stopRecord()
