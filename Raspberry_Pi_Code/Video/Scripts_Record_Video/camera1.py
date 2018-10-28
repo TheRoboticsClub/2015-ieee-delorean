4f8857668f9087c8a89f17d557fce52e0a4e468d
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2018-10-28 22:19:11 +0100

    modifying the resolution and profile to improve quality and compression

--- Raspberry_Pi_Code/Video/Scripts_Record_Video/camera1.py
+++ Raspberry_Pi_Code/Video/Scripts_Record_Video/camera1.py
@@ -12,16 +12,17 @@ destination = '/home/miguel/Vídeos'
 
 
 def cameraSettings():
-    camera.resolution = (1280, 720)
+    camera.resolution = (1920, 1080)
     camera.video_stabilization = True
 
 
 def record():
     filename = os.path.join(destination, dt.datetime.now().strftime('%Y-%m-%d_%H.%M.%S.h264'))
-    camera.start_recording(filename, format='h264', quality = 10)
-
+    camera.start_recording(filename, format='h264', quality = 10,profile = 'high')
+    camera.led = True
 def stopRecord():
     camera.stop_recording(splitter_port = 1)
+    camera.led = False
 
 if __name__ == '__main__':
     ##camera = PiCamera()
