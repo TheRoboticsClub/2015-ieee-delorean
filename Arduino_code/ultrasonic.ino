27128a7b32d35287e54738f87d8208f8eb7fae88
Adrián Rodrigo Castillo <adri_rodri_64@hotmail.es>
2018-10-31 00:48:22 +0100

    Update ultrasonic.ino

--- Arduino_code/ultrasonic.ino
+++ Arduino_code/ultrasonic.ino
@@ -7,27 +7,27 @@ const int alertPin = 1;
 long duration;
 int distance;
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-pinMode(alertPin, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(alertPin, OUTPUT);
 }
 void loop() {
 // Clears the trigPin
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 // Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 // Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
+  duration = pulseIn(echoPin, HIGH);
 // Calculating the distance
-distance= duration*0.034/2;
+  distance= duration*0.034/2;
 // Prints the distance on the Serial Monitor
-if(distance <= 3){
-  digitalWrite(alertPin, HIGH);
-}else{
-  digitalWrite(alertPin, LOW);
-}
-delay(100);
+  if(distance <= 3){
+    digitalWrite(alertPin, HIGH);
+  }else{
+    digitalWrite(alertPin, LOW);
+  }
+  delay(100);
 }
