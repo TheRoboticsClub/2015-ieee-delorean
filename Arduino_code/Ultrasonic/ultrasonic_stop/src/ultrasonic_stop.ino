a42465dce52858b919af2c7608dd1a6d2f7e7f3f
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-01-22 22:07:03 +0100

    Ultrasonics array

--- Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
+++ Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
@@ -1,21 +1,31 @@
 #include <Arduino.h>
 #include <NewPing.h>
 
-
-const int UltrasonicPin = 6;
+const int MaxDistance = 200;
+const int SONAR_NUM = 3;      // Number of sensors.
+/*const int UltrasonicPin = 6;
 const int ECHO_PIN = 5;
 const int MaxDistance = 200;
 
-NewPing sonar(UltrasonicPin, ECHO_PIN, MaxDistance);
+/*NewPing sonar(UltrasonicPin, ECHO_PIN, MaxDistance);
 /*Hagamos un array de ultrasonidos */
+NewPing sonar[SONAR_NUM] = {   // Sensor object array.
+  NewPing(12, 13, MaxDistance), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(10, 9, MaxDistance),
+  NewPing(6, 5, MaxDistance)
+};
 
 
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  delay(100);
-  Serial.print("Ping: ");                      // esperar 50ms entre pings (29ms como minimo)
-  Serial.print(sonar.ping_cm()); // obtener el valor en cm (0 = fuera de rango)
-  Serial.println("cm");
+  for (int i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
+    delay(100); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(sonar[i].ping_cm());
+    Serial.print("cm ");
+  }
+  Serial.println();
 }
