385ef0f589014918aa53687a0970b1e1dee41658
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-01-22 22:25:32 +0100

    Array de ultrasonidos

--- Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
+++ Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
@@ -21,7 +21,7 @@ void setup() {
 }
 void loop() {
   for (int i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
-    delay(100); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     Serial.print(i);
     Serial.print("=");
     Serial.print(sonar[i].ping_cm());
