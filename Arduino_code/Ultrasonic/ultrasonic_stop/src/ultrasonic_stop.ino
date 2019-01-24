d03af974f7b4155b9c060fd10ec4ce29297085b3
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-01-24 20:42:25 +0100

    Añado led, aunque tengo algunos prolemas ya qeu parpadea, tengo que ver como programar bien la señal de salida

--- Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
+++ Arduino_code/Ultrasonic/ultrasonic_stop/src/ultrasonic_stop.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <NewPing.h>
 
+const int ledPIN = 3;
 const int MaxDistance = 200;
 const int SONAR_NUM = 3;      // Number of sensors.
 /*const int UltrasonicPin = 6;
@@ -18,13 +19,23 @@ NewPing sonar[SONAR_NUM] = {   // Sensor object array.
 
 void setup() {
   Serial.begin(9600);
+pinMode(ledPIN , OUTPUT);
 }
 void loop() {
+    /*digitalWrite(ledPIN , LOW);*/
+
   for (int i = 0; i < SONAR_NUM; i++) { // Loop through each sensor and display results.
     delay(50); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
     Serial.print(i);
     Serial.print("=");
-    Serial.print(sonar[i].ping_cm());
+    Serial.print(sonar[1].ping_cm());
+    if (sonar[1].ping_cm()>1 && sonar[1].ping_cm()<15){
+        digitalWrite(ledPIN , HIGH);
+        delay(100);
+    }else{
+        digitalWrite(ledPIN , LOW);
+        delay(100);
+    }
     Serial.print("cm ");
   }
   Serial.println();
