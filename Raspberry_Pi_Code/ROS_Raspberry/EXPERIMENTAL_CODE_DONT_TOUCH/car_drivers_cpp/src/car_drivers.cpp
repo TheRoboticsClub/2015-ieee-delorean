39e936ac0332fc1d4d69d4e320b05d7a5b8d0899
Adrián Rodrigo Castillo <adri_rodri_64@hotmail.es>
2019-11-23 18:36:18 +0100

    updated

--- Raspberry_Pi_Code/ROS_Raspberry/EXPERIMENTAL_CODE_DONT_TOUCH/car_drivers_cpp/src/car_drivers.cpp
+++ Raspberry_Pi_Code/ROS_Raspberry/EXPERIMENTAL_CODE_DONT_TOUCH/car_drivers_cpp/src/car_drivers.cpp
@@ -2,22 +2,25 @@
 #include "std_msgs/String.h"
 #include "geometry_msgs/Twist.h"
 #include <sstream>
+#include <time.h>
+#include <thread>
+using namespace std;
 
 
-class pubSubscriber{
+class vel_pub_subscriber{
 
 public:
 
-  pubSubscriber(ros::NodeHandle n, ros::Publisher pub)
+  vel_pub_subscriber(ros::NodeHandle n, ros::Publisher pub)
   {
-    n_ = n
+    n_ = n;
     pub_ = pub;
-    sub_ = n.subscribe("/car1/cmd_vel", 10, &pubSubscriber::cmd_vel_callback, this);
+    sub_ = n_.subscribe("/car1/cmd_vel", 10, &vel_pub_subscriber::cmd_vel_callback, this);
   }
 
   void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
   {
-    ROS_INFO("CMD_VEL CALLBACK [%s]", msg->data.c_str());
+    ROS_INFO("CMD_VEL CALLBACK [%f]", msg->linear.x);
     pub_.publish(msg);
   }
 
@@ -29,6 +32,75 @@ private:
 };
 
 
+class Timer{
+
+public:
+
+  bool arrived;
+  double dt;
+  time_t old_time;
+  time_t time_now;
+
+  Timer(){
+
+    dt = 100;
+    arrived = false;
+    time(&old_time);
+    time(&time_now);
+
+  }
+
+};
+
+
+class ping_subscriber{
+
+public:
+
+  ping_subscriber(ros::NodeHandle n, Timer& timer){
+
+    n_ = n;
+    sub_ = n_.subscribe("/ping", 10, &ping_subscriber::ping_callback, this);
+    timer_ = timer;
+
+  }
+
+  void ping_callback(const std_msgs::String::ConstPtr& msg){
+    timer_.arrived = true;
+  }
+
+private:
+
+  ros::NodeHandle n_;
+  ros::Subscriber sub_;
+  Timer timer_;
+
+};
+
+void ping_verifier(Timer& timer){
+
+
+  while(1){
+
+    if(timer.arrived){
+
+      timer.old_time = timer.time_now;
+      timer.arrived = false;
+    }
+
+    time(&timer.time_now);
+    timer.dt = difftime(timer.time_now, timer.old_time);
+
+    if(timer.dt >= 3){
+      __asm__("nop");//todo
+    }
+
+
+  }
+
+
+}
+
 
 int main(int argc, char **argv)
 {
@@ -37,7 +109,14 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
   ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/arduino/cmd_vel", 10);
-  pubSubscriber subscriber(n, pub);
+
+  Timer timer;
+  vel_pub_subscriber vel_subscriber(n, pub);
+  ping_subscriber ping_sub(n, timer);
+
+  auto ping_thread = std::thread(ping_verifier, std::ref(timer));
+
+  ros::spin();
 
   return 0;
 }
