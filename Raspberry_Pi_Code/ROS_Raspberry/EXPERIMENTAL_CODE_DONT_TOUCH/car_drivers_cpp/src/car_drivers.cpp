b6043a3f8aee4ca5e3fc346fad9687c7e60696b1
Adrián Rodrigo Castillo <adri_rodri_64@hotmail.es>
2019-11-30 01:03:46 +0100

    Add files via upload

--- Raspberry_Pi_Code/ROS_Raspberry/EXPERIMENTAL_CODE_DONT_TOUCH/car_drivers_cpp/src/car_drivers.cpp
+++ Raspberry_Pi_Code/ROS_Raspberry/EXPERIMENTAL_CODE_DONT_TOUCH/car_drivers_cpp/src/car_drivers.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <time.h>
 #include <thread>
+#include <chrono>
 using namespace std;
 
 
@@ -40,6 +41,7 @@ public:
   double dt;
   time_t old_time;
   time_t time_now;
+  int debug_int;
 
   Timer(){
 
@@ -47,6 +49,7 @@ public:
     arrived = false;
     time(&old_time);
     time(&time_now);
+    debug_int = 0;
 
   }
 
@@ -57,7 +60,7 @@ class ping_subscriber{
 
 public:
 
-  ping_subscriber(ros::NodeHandle n, Timer& timer){
+  ping_subscriber(ros::NodeHandle n, Timer *timer){
 
     n_ = n;
     sub_ = n_.subscribe("/ping", 10, &ping_subscriber::ping_callback, this);
@@ -66,19 +69,20 @@ public:
   }
 
   void ping_callback(const std_msgs::String::ConstPtr& msg){
-    timer_.arrived = true;
+    //ROS_INFO("PING ARRIVED");
+    timer_->arrived = true;
+    ROS_INFO("[%i]", timer_->debug_int);
   }
 
 private:
 
   ros::NodeHandle n_;
   ros::Subscriber sub_;
-  Timer timer_;
+  Timer *timer_;
 
 };
 
-void ping_verifier(Timer& timer){
-
+void ping_verifier(Timer& timer, ros::Publisher& vel_pub){
 
   while(1){
 
@@ -86,18 +90,27 @@ void ping_verifier(Timer& timer){
 
       timer.old_time = timer.time_now;
       timer.arrived = false;
+     // ROS_INFO("INSIDE CONDITION");
     }
 
     time(&timer.time_now);
+    //ROS_INFO("TIME NOW: [%f]", timer.time_now);
+    //ROS_INFO("OLD TIME: [%f]", timer.old_time);
     timer.dt = difftime(timer.time_now, timer.old_time);
+    ROS_INFO("DT is: [%f]", timer.dt);
 
     if(timer.dt >= 3){
-      __asm__("nop");//todo
+      //ROS_INFO("PING DINT'T ARRIVE IN TIME");
+      geometry_msgs::Twist t_msg;
+      t_msg.linear.x = 0.5;
+      t_msg.angular.z = 0.5;
+      vel_pub.publish(t_msg);
+
     }
 
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
   }
-
 
 }
 
@@ -112,9 +125,9 @@ int main(int argc, char **argv)
 
   Timer timer;
   vel_pub_subscriber vel_subscriber(n, pub);
-  ping_subscriber ping_sub(n, timer);
+  ping_subscriber ping_sub(n, &timer);
 
-  auto ping_thread = std::thread(ping_verifier, std::ref(timer));
+  auto ping_thread = std::thread(ping_verifier, std::ref(timer), std::ref(pub));
 
   ros::spin();
 
